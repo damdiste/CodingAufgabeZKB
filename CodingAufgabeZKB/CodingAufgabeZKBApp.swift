0633a65b072ff6ecad36460e954073d57b4761a7
damdiste <stella.damdin@gmail.com>
2023-12-01 11:45:54 +0100

    Initial Commit

---
+++ CodingAufgabeZKB/CodingAufgabeZKBApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodingAufgabeZKBApp.swift
+//  CodingAufgabeZKB
+//
+//  Created by Stella Damdin on 01.12.2023.
+//
+
+import SwiftUI
+
+@main
+struct CodingAufgabeZKBApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
